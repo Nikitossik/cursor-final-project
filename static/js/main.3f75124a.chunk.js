47bdec7df705f03baf54f2e52a754d3894de6c72
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-16 15:07:32 +0300

    Updates

---
+++ static/js/main.3f75124a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcursor-final-project"]=this["webpackJsonpcursor-final-project"]||[]).push([[0],{10:function(t,e,c){"use strict";c.r(e);var n=c(1),r=c.n(n),s=c(3),o=c.n(s),i=(c(8),c(0));var a=function(){return Object(i.jsx)("div",{className:"App"})};o.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.3f75124a.chunk.js.map
