5f9c46ad6db90874164460c95f9da82c1bfe8fa6
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-17 18:04:55 +0300

    created global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import HomePage from "./components/Homepage";
+import { createGlobalStyle } from 'styled-components';
+
+const Global = createGlobalStyle`
+  *, *::before, *::after{
+    box-sizing: border-box;
+  }
+
+  body{
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif
+  }
+`
 
 function App() {
   return (
     <div className="App">
+      <Global />
       <HomePage />
     </div>
   );
