763899d46d341f2ce7f5142d6abfc7d1a0279098
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-17 18:05:08 +0300

    added money prop

--- src/components/data.js
+++ src/components/data.js
@@ -3,49 +3,57 @@ const charges = [
         index: 0,
         category: 'Food',
         description: 'Demesne far hearted suppose ',
-        date: '27 August 2021'
+        date: '27 August 2021',
+        money: '5$'
     },
     {
         index: 1,
         category: 'Clothes',
         description: 'Demesne far ',
-        date: '27 August 2021'
+        date: '27 August 2021',
+        money: '6$'
     },
     {
         index: 2,
         category: 'Restaurants',
         description: 'Demesne far hearted suppose venture',
-        date: '27 August 2021'
+        date: '27 August 2021',
+        money: '10$'
     },
     {
         index: 3,
         category: 'Food',
         description: 'Demesne far hearted suppose see had has',
-        date: '27 August 2021'
+        date: '27 August 2021',
+        money: '5$'
     },
     {
         index: 4,
         category: 'Clothes',
         description: 'Demesne far heartedhad has',
-        date: '27 August 2021'
+        date: '27 August 2021',
+        money: '5$'
     },
     {
         index: 5,
         category: 'Food',
         description: 'Demesne faxcited see had has',
-        date: '27 August 2021'
+        date: '27 August 2021',
+        money: '5$'
     },
     {
         index: 6,
         category: 'Clothes',
         description: 'Demesne far hearted suppose',
-        date: '27 August 2021'
+        date: '27 August 2021',
+        money: '5$'
     },
     {
         index: 7,
         category: 'Food',
         description: 'Demesne far hearted venture excited see had has',
-        date: '27 August 2021'
+        date: '27 August 2021',
+        money: '5$'
     }
 ];
 
