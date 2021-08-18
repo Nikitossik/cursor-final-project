6d9e12b1442fdfa743d1e2ba6b3da7df1de60247
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-18 21:06:14 +0300

    added SectionTitle, Tab(Nav/Link) styled components

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import wallet from "../image/wallet1.jpg";
+import wallet from "../assets/wallet1.jpg";
+import { NavLink, Link } from 'react-router-dom';
 
 export const HeaderWrapper = styled.div`
   width: 100%;
@@ -16,6 +17,7 @@ export const HeaderBalanceBlock = styled.div`
   width: 150px;
   height: 100%;
 `;
+
 export const HeaderContent = styled.div`
   padding: 50px 10px;
   color: #f9e8b4;
@@ -103,7 +105,6 @@ export const InputGroup = styled.div`
 `;
 
 export const Button = styled.button`
-  margin-top: 10px;
   border-radius: 4px;
   border: 1px solid #222;
   padding: 10px 15px;
@@ -111,4 +112,66 @@ export const Button = styled.button`
   font-size: 1rem;
   background-color: #f9e8b4;
   color: #222;
+`;
+
+export const AddButton = styled(Link)`
+  width: 120px;
+  min-width: 120px;
+  border-radius: 4px;
+  border: 1px solid #222;
+  padding: 10px 15px;
+  width: 100%;
+  font-size: 1rem;
+  background-color: #f9e8b4;
+  color: #222;
+  text-decoration: none;
+`;
+
+export const SectionTitle = styled.h2`
+  font-size: 1.2rem;
+  color: #333;
+  line-height: 1.5;
+  margin: 10px 0 40px 0;
+`;
+
+export const TabNav = styled.nav`
+  width: 100%;
+  height: 50px;
+  background-color: #C4E2F2;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const TabLink = styled(NavLink)`
+  min-width: 200px;
+  padding: 15px 30px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 1.1rem;
+  color: rgba(0,0,0,0.7);
+  position: relative;
+  background: #fff;
+  display: inline-block;
+  height: 100%;
+  
+  &::before{
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 3px;
+    transform: scaleX(0);
+    background: #52BAF2;
+  }
+
+  &.${props => props.activeClassName}{
+    color: #000;
+
+    &::before{
+      transform: scale(1);
+    }
+
+  }
 `;
