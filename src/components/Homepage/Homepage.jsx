166a9b8b0319407d5f18838b8093d2193f98b0a1
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-26 17:55:54 +0300

    changed index.jsx to HomePage.jsx

---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,30 @@
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import BalanceHeader from '../BalanceHeader';
+
+import Charges from './Charges';
+import Incomes from './Incomes';
+
+import {TabNav, TabLink} from '../styles';
+
+function HomePage(){
+    return (
+        <section className='homepage page'>
+            <BalanceHeader/>
+            <Router basename='/home'>
+
+             <TabNav>
+                <TabLink exact to='/charges' activeClassName='tab--active'>Charges</TabLink>
+                <TabLink to='/incomes' activeClassName='tab--active'>Incomes</TabLink>
+            </TabNav>
+
+            <Switch>
+                <Route path='/charges' component={Charges}/>
+                <Route path='/incomes' component={Incomes}/>
+            </Switch>
+        </Router>
+        </section>
+    );
+}
+
+export default HomePage;
