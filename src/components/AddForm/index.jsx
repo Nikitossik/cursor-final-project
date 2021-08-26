b23dce8e693eacb73e9ae4d029faf9610c74494a
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-26 17:54:30 +0300

    added input and click handlers to dispatch actions to store

--- src/components/AddForm/index.jsx
+++ src/components/AddForm/index.jsx
@@ -1,22 +1,74 @@
-import {AddFormWrapper,StyledAddForm, InputGroup, Button} from '../styles';
-
+import {AddFormWrapper, StyledAddForm, InputGroup, Button} from '../styles';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { add as addCharge} from '../../redux/chargesSlice';
+import { add as addIncome} from '../../redux/incomesSlice';
 
 function AddForm({active, setActive, title}) {
+
+    const dispatch = useDispatch();
+
+    const [money, setMoney] = useState(0);
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [date, setDate] = useState('');
+
+    const handleInput = e => {
+        const {name, value} = e.target;
+
+        switch (name) {
+            case 'total-sum':
+                setMoney(value);
+                break;
+            case 'description':
+                setDescription(value);
+                break;
+            case 'category':
+                setCategory(value);
+                break;
+            case 'date':
+                setDate(value);
+                break;       
+            default:
+                break;
+        }
+    }
+
+    const handleClick = e => {
+        e.preventDefault();
+        if (title === 'income'){
+            dispatch(addIncome({
+                money, 
+                description,
+                category,
+                date
+            }));
+        }
+        else if (title === 'charge'){
+            dispatch(addCharge({
+                money, 
+                description,
+                category,
+                date
+            }));
+        }
+    }
+
     return (
         <AddFormWrapper className={active ? "active": "inactive"} onClick={() => setActive(false)}>
         <StyledAddForm onClick={e =>e.stopPropagation()}>
                 <h1>Add new {title}</h1>
             <InputGroup>
                 <label htmlFor='total-sum' className="input-group__label">Total</label>
-                <input name='total-sum' id='total-sum' type="number" min='0' className='form-input'/>
+                <input onInput={handleInput} name='total-sum' id='total-sum' type="number" min='0' className='form-input'/>
             </InputGroup>
             <InputGroup>
                 <label htmlFor='description' className="input-group__label">Description</label>
-                <input name='description' id='description' type="text" className='form-input'/>
+                <input onInput={handleInput} name='description' id='description' type="text" className='form-input'/>
             </InputGroup>
             <InputGroup>
                 <label htmlFor='category' className="input-group__label">Select category</label>
-                <select name='category' id='category' className='form-select'>
+                <select onChange={handleInput} name='category' id='category' className='form-select'>
                     <option value="Option 1">Option 1</option>
                     <option value="Option 2">Option 2</option>
                     <option value="Option 3">Option 3</option>
@@ -27,9 +79,9 @@ function AddForm({active, setActive, title}) {
             </InputGroup>
             <InputGroup>
                 <label htmlFor='date' className="input-group__label">Select Date</label>
-                <input name='date' id='date' type="date" className='form-input'/>
+                <input onInput={handleInput} name='date' id='date' type="date" className='form-input'/>
             </InputGroup>
-            <Button>Add new {title}</Button>
+            <Button onClick={handleClick}>Add new {title}</Button>
         </StyledAddForm>
         </AddFormWrapper>
     );
