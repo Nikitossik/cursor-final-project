91a069db6ed6b371004a55719d152869ffc97fcf
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-26 17:53:35 +0300

    created app store

---
+++ src/store.js
@@ -0,0 +1,26 @@
+// import thunk from "redux-thunk";
+// import { createStore, applyMiddleware, combineReducers } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import addChargeReducer from "./redux/reducers/add-charge-reducer";
+
+// const reducer = combineReducers({
+//     addChargeReducer
+// })
+
+// const store = createStore(
+//     reducer,
+//     composeWithDevTools(applyMiddleware(thunk))
+// )
+
+// export default store;
+
+import { configureStore } from '@reduxjs/toolkit';
+import chargesReducer from './redux/chargesSlice';
+import incomesReducer from './redux/incomesSlice';
+
+export default configureStore({
+    reducer: {
+        charges: chargesReducer,
+        incomes: incomesReducer
+    },
+});
