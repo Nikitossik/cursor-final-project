991a060fdafa23b79575597abe781fc6f3e74d43
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-26 17:53:18 +0300

    removed root router, added Provider for store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from "react-redux";
+import store from './store.js';
 
 ReactDOM.render(
-  <Router path='/' exact>
+  <Provider store={store}>
     <App />
-  </Router>,
+  </Provider>,
   document.getElementById('root')
 );
