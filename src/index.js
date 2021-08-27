e94e8db64322522e2e3eb1c789de1b4d0c02068f
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-27 17:19:05 +0300

    added hydrate action

--- src/index.js
+++ src/index.js
@@ -2,10 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
-import configureStore from "./store.js";
+import store from "./store.js";
+
+import { hydrateCharges } from './redux/chargesSlice';
+import { hydrateIncomes } from './redux/incomesSlice';
+
+const getItemFromLocalStorage = (item) => {
+  try {
+    const persistedState = localStorage.getItem('reduxState')[item];
+    if (persistedState)
+      return JSON.parse(persistedState)
+  }
+  catch (e) {
+    console.log(e)
+  }
+}
+
+const charges = getItemFromLocalStorage('charges');
+const incomes = getItemFromLocalStorage('incomes');
+
+if (charges) {
+  store.dispatch(hydrateCharges(charges))
+}
+
+if (incomes) {
+  store.dispatch(hydrateIncomes(incomes))
+}
+
 
 ReactDOM.render(
-  <Provider store={configureStore}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
