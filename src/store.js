65ad29a40716e2cee1c881b642b04aa3792e2e2c
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-27 17:19:39 +0300

    setting info to localStorage from store

--- src/store.js
+++ src/store.js
@@ -1,26 +1,10 @@
-// import thunk from "redux-thunk";
-// import { createStore, applyMiddleware, combineReducers } from "redux";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import addChargeReducer from "./redux/reducers/add-charge-reducer";
-
-// const reducer = combineReducers({
-//     addChargeReducer
-// })
-
-// const store = createStore(
-//     reducer,
-//     composeWithDevTools(applyMiddleware(thunk))
-// )
-
-// export default store;
-
 import { configureStore } from "@reduxjs/toolkit";
 import chargesReducer from "./redux/chargesSlice";
 import incomesReducer from "./redux/incomesSlice";
 import chargesCategoriesReducer from "./redux/chargesCategoriesSlice";
 import incomesCategoriesReducer from "./redux/incomesCategoriesSlice";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     charges: chargesReducer,
     incomes: incomesReducer,
@@ -28,3 +12,10 @@ export default configureStore({
     incomesCategories: incomesCategoriesReducer,
   },
 });
+
+store.subscribe(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+});
+
+
+export default store;
