ca032594cf564774e523a13fea5662f0efd8d6bf
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-27 17:22:01 +0300

    added conditional rendering, grabbing from localStorage

--- src/components/Homepage/Charges.jsx
+++ src/components/Homepage/Charges.jsx
@@ -2,19 +2,38 @@ import React, { useState} from "react";
 import Table from './Table';
 import { AddButton } from '../styles';
 import AddForm from '../AddForm';
-import { useSelector } from 'react-redux';
-import { selectCharges } from '../../redux/chargesSlice';
+import MessageEmpty from "./MessageEmpty";
+
 
 function Charges() {
-    const [formActive, setFormActive] = useState(false);
+    const messageText = 'You don\'t have any charges yet';
+    const reduxState = JSON.parse(localStorage.getItem('reduxState'));
+    const localCharges = reduxState ? reduxState.charges : [];
+    const [renderData, setRenderData] = useState(localCharges);
 
-    const charges = useSelector(selectCharges);
+    const getChargesFromLocalStorage = e => {
+        const reduxState = JSON.parse(localStorage.getItem('reduxState'));
+        const localCharges = reduxState ? reduxState.charges : [];
+        setRenderData(localCharges);
+    }
+
+    const [formActive, setFormActive] = useState(false);
 
     return (
             <section className='charges-section section'>
                 <AddButton onClick={() => setFormActive(true)}>Add more charges</AddButton>
-                <Table rows={charges}/>
-                <AddForm active={formActive} setActive={setFormActive} title={"charge"}/>
+                {
+                    renderData.length !== 0 ? 
+                        <Table data={renderData}/> 
+                        : 
+                        <MessageEmpty messageText={messageText}/>
+                }
+                <AddForm 
+                    active={formActive} 
+                    setActive={setFormActive} 
+                    title="charge" 
+                    parentHandler={getChargesFromLocalStorage}
+                />
             </section>
     )
 }
--- src/components/Homepage/Incomes.jsx
+++ src/components/Homepage/Incomes.jsx
@@ -2,20 +2,38 @@ import React, { useState} from "react";
 import Table from './Table';
 import { AddButton } from '../styles';
 import AddForm from '../AddForm';
-import { useSelector } from 'react-redux';
-import { selectIncomes } from '../../redux/incomesSlice';
+import MessageEmpty from "./MessageEmpty";
 
 
 function Incomes() {
+    const messageText = 'You don\'t have any incomes yet';
+    const reduxState = JSON.parse(localStorage.getItem('reduxState'));
+    const localIncomes = reduxState ? reduxState.incomes : [];
+    const [renderData, setRenderData] = useState(localIncomes);
+
+    const getIncomesFromLocalStorage = e => {
+        const reduxState = JSON.parse(localStorage.getItem('reduxState'));
+        const localIncomes= reduxState ? reduxState.incomes : [];
+        setRenderData(localIncomes);
+    }
+
     const [formActive, setFormActive] = useState(false);
 
-    const incomes = useSelector(selectIncomes);
-
     return (
-        <section className='charges-section section'>
-            <AddButton onClick={() => setFormActive(true)}>Add more incomes</AddButton>
-            <Table rows={incomes}/>
-            <AddForm active={formActive} setActive={setFormActive} title={"income"}/>
+        <section className='incomes-section section'>
+           <AddButton onClick={() => setFormActive(true)}>Add more incomes</AddButton>
+                {
+                    renderData.length !== 0 ? 
+                        <Table data={renderData}/> 
+                        : 
+                        <MessageEmpty messageText={messageText}/>
+                }
+                <AddForm 
+                    active={formActive} 
+                    setActive={setFormActive} 
+                    title="income"
+                    parentHandler={getIncomesFromLocalStorage}
+                />
         </section>
     )
 }
