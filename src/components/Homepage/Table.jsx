4ae0da3915dce20c121d426e70e73b2aefa45741
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-29 17:03:46 +0300

    added sorting functionality

--- src/components/Homepage/Table.jsx
+++ src/components/Homepage/Table.jsx
@@ -1,28 +1,80 @@
+import React from 'react';
 import TableRow from "./TableRow";
 import {TableWrapper, StyledTable}  from '../styles';
 
-const Table = ({data}) => {
+class Table extends React.Component {
 
-    return (
-        <TableWrapper>
-            <StyledTable>
-                <thead>
-                    <tr>
-                        <th>Category</th>
-                        <th>Description</th>
-                        <th>Date</th>
-                        <th>Money</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((row) => (
-                        <TableRow {...row} key={row.id} />
-                    ))}
-                </tbody>
-            </StyledTable>
-        </TableWrapper>
-    );
-};
+    sortByText = (data, isReverse, option) => {
+        const bigger = isReverse ? -1 : 1;
+        const less = isReverse ? 1 : -1;
+        data.sort((a, b) => {    
+            const first = a[option].toLowerCase();
+            const second = b[option].toLowerCase();
+            return first > second ? bigger : first === second ? 0 : less;
+        });
+    }
+
+    sortByMoney = (data, isReverse) => {
+        const bigger = isReverse ? -1 : 1;
+        const less = isReverse ? 1 : -1;
+        data.sort((a, b) => {    
+            const first = +a.money;
+            const second = +b.money;
+            return first > second ? bigger : first === second ? 0 : less;
+        });
+    }
+
+    sortByDates = (data, isReverse) => {
+        const bigger = isReverse ? -1 : 1;
+        const less = isReverse ? 1 : -1;
+        data.sort((a, b) => {    
+            const first = Date.parse(a.date);
+            const second = Date.parse(b.date);
+            return first > second ? bigger : first === second ? 0 : less;
+        });
+    }
+
+    sort = (data, sortOption, isReverse) => {
+        if (sortOption === 'category' || sortOption === 'description') {
+            this.sortByText(data, isReverse, sortOption);
+        }
+        else if (sortOption === 'money') {
+            this.sortByMoney(data, isReverse);
+        }
+        else if (sortOption === 'date') {
+            this.sortByDates(data, isReverse);
+        }
+    }
+
+    render() {
+        const {renderData} = this.props;
+        const {sortOption, isReverse} = this.props.sortParams;
+
+        let resultData = [...renderData];
+
+        if (sortOption !== '') this.sort(resultData, sortOption, isReverse);
+
+        const rows = resultData.map(row => <TableRow {...row} key={row.id} />);
+
+        return (
+            <TableWrapper>
+                <StyledTable>
+                    <thead>
+                        <tr>
+                            <th>Category</th>
+                            <th>Description</th>
+                            <th>Date</th>
+                            <th>Money</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows}
+                    </tbody>
+                </StyledTable>
+            </TableWrapper>
+        )
+    };
+}
 
 export default Table;
