083554e01a46d6f10408e982b5d3cbb86280c12b
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-29 20:18:49 +0300

    some editions

--- src/redux/incomesSlice.js
+++ src/redux/incomesSlice.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 const reduxState = JSON.parse(localStorage.getItem('reduxState'));
 let initialState = [];
 
-if (reduxState) initialState = reduxState.incomes;
+if (reduxState) initialState = reduxState.incomes || [];
 
 export const slice = createSlice({
     name: 'incomes',
