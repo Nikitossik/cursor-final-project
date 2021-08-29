8871adbaa3ce0926ccbdd0461294c36e42b575af
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-29 20:17:44 +0300

    render balance

--- src/components/BalanceHeader/index.jsx
+++ src/components/BalanceHeader/index.jsx
@@ -1,12 +1,14 @@
 import {HeaderWrapper, HeaderBalanceBlock, HeaderContent, HeaderBalance}  from '../styles';
+import {useState, useEffect} from 'react';
 
-const BalanceHeader = () => {
+const BalanceHeader = ({balance}) => {
+
     return (
         <HeaderWrapper>
             <HeaderBalanceBlock>
                 <HeaderContent>
                     <h5>Your balance</h5>
-                    <HeaderBalance>$ 5000.00</HeaderBalance>
+                    <HeaderBalance>{balance} $</HeaderBalance>
                 </HeaderContent>
             </HeaderBalanceBlock>
         </HeaderWrapper>
