0541a7c410595cd46909c7e8e395cd6dc2e00aca
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-31 15:26:53 +0300

    rendering categories

--- src/components/Categories/CategoryCard.jsx
+++ src/components/Categories/CategoryCard.jsx
@@ -1,26 +1,24 @@
 import { CategoryCardWrapper } from '../styles';
-import { useSelector } from 'react-redux';
-import { selectChargesCategories } from '../../redux/chargesCategoriesSlice';
 
-const CategoryCard = (group) => {
+const CategoryCard = ({value, label, background, categories}) => {
 
-    const chargesCategories = useSelector(selectChargesCategories);
     let categoryList = [];
-    categoryList = chargesCategories.filter(category => {
-                    return (category.groupCategory === group.value)
-                });
+    categoryList = categories.filter(category => {
+        return (category.groupCategory === value)
+    });
 
     return (
         <CategoryCardWrapper>
-                        <img src={group.background} alt="img21"/>
-                        <div className="card-content">
-                            <h3>{group.label}</h3>
+            <img src={background} alt="img21"/>
+            <div className="card-content">
+                <h3>{label}</h3>
                 <ul>
                     {categoryList.map((category) => (
-                        <li {...category} key={category.id}>{category.label}</li>))}
-                                </ul>
-                        </div>           
-                    </CategoryCardWrapper>
+                        <li {...category} key={category.id}>{category.label}</li>))
+                    }
+                 </ul>
+            </div>           
+        </CategoryCardWrapper>
     );
 };
 
