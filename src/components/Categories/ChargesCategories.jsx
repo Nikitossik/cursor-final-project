1f0967bf20e6bb119a6ac5a7429112da8da9d0e3
Nikita Rahmany <nikita.rahmany@gmail.com>
2021-08-31 15:27:53 +0300

    separated data from components in file

--- src/components/Categories/ChargesCategories.jsx
+++ src/components/Categories/ChargesCategories.jsx
@@ -1,108 +1,38 @@
-import React, { useState, useEffect} from "react";
-import { AddButton, CategoryBlock} from '../styles';
+import React, { useState, useEffect } from "react";
+import { AddButton, CategoryBlock } from '../styles';
 import AddCategoryForm from '../AddCategoryForm';
 import CategoryCard from './CategoryCard';
 import { useSelector } from 'react-redux';
 import { selectChargesCategories } from '../../redux/chargesCategoriesSlice';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import CLOTHING from "../../assets/clothing.jpg";
-import AUTO from "../../assets/auto.jpg";
-import FOOD from "../../assets/food.jpg";
-import PAYMENTS from "../../assets/payments.jpg";
-import EDUCATION from "../../assets/education.jpg";
-import HOUSEWARE from "../../assets/houseware.jpg";
-import HEALTH from "../../assets/med.jpg";
-import {
-  faUtensils,
-  faCar,
-  faUserGraduate,
-  faTshirt,
-  faFileInvoiceDollar,
-  faMedkit,
-  faLaptopHouse,
-} from "@fortawesome/free-solid-svg-icons";
+import { charges_groups } from "./groupCategories";
 
 function ChargesCategories() {
-    const groupCategories = [
-  {
-    id: 1,
-    value: 'food',
-    label: `Food`,
-    icon: <FontAwesomeIcon icon={faUtensils} />,
-    background: FOOD
-  },
-  {
-    id: 2,
-    value: "clothing-and-shoes",
-    label: "Сlothing & Shoes",
-    icon: <FontAwesomeIcon icon={faTshirt} />,
-    background: CLOTHING,
-  },
-  {
-    id: 3,
-    value: 'education-and-vacation',
-    label: "Education & Vacation",
-    icon: <FontAwesomeIcon icon={faUserGraduate} />,
-    background: EDUCATION
-  },
-  {
-    id: 4,
-    value: "car-and-transport-services",
-    label: "Сar & Transport Services",
-      icon: <FontAwesomeIcon icon={faCar} />,
-    background: AUTO,
-  },
-  {
-    id: 5,
-    value: "bill-payments",
-    label: "Bill Payments",
-    icon: <FontAwesomeIcon icon={faFileInvoiceDollar}/>,
-    background: PAYMENTS,
-  },
-  {
-    id: 6,
-    value: "health-and-beauty",
-    label: "Health & Beauty",
-    icon: <FontAwesomeIcon icon={faMedkit} />,
-    background: HEALTH,
-  },
-  {
-    id: 7,
-    value: "household-appliances-and-homeware",
-    label: "Household Appliances & Homeware",
-    icon: <FontAwesomeIcon icon={faLaptopHouse} />,
-    background: HOUSEWARE,
-  },
-  ];
-  
+
   const [formActive, setFormActive] = useState(false);
-  // const [categories, setCategories] = useState(chargesCategories);
 
-  // const getChargesCategoriesFromLocalStorage = () => {
-  //   const reduxState = JSON.parse(localStorage.getItem('reduxState'));
-  //   const localCategories = reduxState ? reduxState.ChargesCategories : categories;
-  //   setCategories(localCategories);
-  // }
+  const categories= useSelector(selectChargesCategories);
 
-  // useEffect(() => {
-  //   getChargesCategoriesFromLocalStorage();
-  // }, []);
+  return (
+    <section className='charges-section section'>
+      <AddButton onClick={() => setFormActive(true)}>Add more charges categories</AddButton>
+      <div>
+        <CategoryBlock>
+          {
+            charges_groups.map(
+              group => <CategoryCard {...group} key={group.id} categories={categories}/>
+            )
+          }
+        </CategoryBlock>
+      </div>
 
-
-    return (
-            <section className='charges-section section'>
-            <AddButton onClick={() => setFormActive(true)}>Add more charges categories</AddButton>
-            <div>
-                <CategoryBlock>
-                    {groupCategories.map((group) => (
-                      <CategoryCard {...group} key={group.id}></CategoryCard>
-          ))}
-                </CategoryBlock>
-            </div>
-
-            <AddCategoryForm active={formActive} setActive={setFormActive} groupCategories={groupCategories}/>
-            </section>
-    )
+      <AddCategoryForm
+        active={formActive}
+        setActive={setFormActive}
+        groupCategories={charges_groups}
+        title='chargesCategories'
+      />
+    </section>
+  )
 }
 
 export default ChargesCategories;
--- src/components/Categories/IncomesCategories.jsx
+++ src/components/Categories/IncomesCategories.jsx
@@ -1,67 +1,36 @@
-import React, { useState} from "react";
+import React, { useState, useEffect} from "react";
 import { AddButton, CategoryBlock} from '../styles';
 import AddCategoryForm from '../AddCategoryForm';
 import CategoryCard from './CategoryCard';
 import { useSelector } from 'react-redux';
 import { selectIncomesCategories } from '../../redux/incomesCategoriesSlice';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faCreditCard,
-  faPercent,
-  faPiggyBank,
-  faGift,
-} from "@fortawesome/free-solid-svg-icons";
-import INTEREST from "../../assets/interest.jpg";
-import RENT from "../../assets/rent.jpg";
-import SALARY from "../../assets/salary.jpg";
-import OTHER from "../../assets/other.jpg";
+
+import { incomes_groups } from './groupCategories';
 
 function IncomesCategories() {
-    const groupCategories = [
-  {
-    id: 1,
-    value: 'salary-and-pension',
-            label: "Salary & Pension",
-            icon: <FontAwesomeIcon icon={faCreditCard} />,
-    background: SALARY,
-  },
-  {
-    id: 2,
-    value: 'interest-income',
-    label: "Interest Income",
-      icon: <FontAwesomeIcon icon={faPercent} />,
-    background: INTEREST,
-  },
-  {
-    id: 3,
-    value: 'rental-and-sale-income',
-    label: "Rental & Sale Income",
-      icon: <FontAwesomeIcon icon={faPiggyBank} />,
-    background: RENT,
-  },
-  {
-    id: 4,
-    value: 'other-incomes',
-    label: "Other Incomes",
-      icon: <FontAwesomeIcon icon={faGift} />,
-    background: OTHER,
-  }
-];
+  
     const [formActive, setFormActive] = useState(false);
-
-    const chargesCategories = useSelector(selectIncomesCategories);
+    
+    const categories= useSelector(selectIncomesCategories);
 
     return (
             <section className='charges-section section'>
                 <AddButton onClick={() => setFormActive(true)}>Add more incomes categories</AddButton>
                 <div>
                 <CategoryBlock>
-                    {groupCategories.map((group) => (
-            <CategoryCard {...group} key={group.id}></CategoryCard>
-          ))}
+                    {
+                        incomes_groups.map(
+                            group => <CategoryCard {...group} key={group.id} categories={categories}/>
+                        )
+                    }
                 </CategoryBlock>
             </div>
-                <AddCategoryForm active={formActive} setActive={setFormActive} title={"incomesCategories"} groupCategories={groupCategories}/>
+                <AddCategoryForm 
+                  active={formActive} 
+                  setActive={setFormActive} 
+                  groupCategories={incomes_groups}
+                  title="incomesCategories"
+                />
             </section>
     )
 }
